4c222b8586401f3a32956aaf4df63a08110299be
Belal ElRashidy <80958450+BelalElrashidy@users.noreply.github.com>
2024-10-21 13:19:40 +0300

    Lab 2

--- main.js
+++ main.js
@@ -1,55 +1,75 @@
 function triangle(){
-    const canvas = document.getElementById("square")
-    if (!canvas){
-        alert('could not find HTML canvas element')
-        return
+    const canvas = document.getElementById('square');
+    if (!canvas) {
+      alert('Could not find HTML canvas element - check for typos, or loading JavaScript file too early');
+      return;
     }
-    const gl= canvas.getContext('webgl2');
-    if(!gl){
-        alert("gl isn't found")
-        return
+    const gl = canvas.getContext('webgl2');
+    if (!gl) {
+      alert("WebGL is not supported");
+      return;
     }
-    const vertexData=[
-        0.0,0.5,-0.5,-0.5,0.5,-0.5
-    ]
-    const triangleGeoBuffer=gl.createBuffer()
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER,triangleGeoBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertexData),gl.STATIC_DRAW)
+
+    const vertexData = [
+       0.0, 0.5,    1.0, 0.0, 0.0,  
+      -0.5, -0.5,   0.0,  0.0,1.0,  
+       0.5, -0.5,   0.0, 1.0, 0.0   
+    ];
+
+    const triangleGeoBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleGeoBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
     const vertexShaderSourceCode = `#version 300 es
-    precision mediump float;
+    precision mediump float; 
     
-    in vec2 vertexPostion;
+    in vec2 vertexPosition;  
+    in vec3 vertexColor;     
+    out vec3 fragColor;      
     
-    void main(){
-        gl_Position = vec4(vertexPosition,0.0,1.0);
+    void main() {
+      gl_Position = vec4(vertexPosition, 0.0, 1.0);  
+      fragColor = vertexColor;                       
     }`;
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER)
-    gl.shaderSource(vertexShader,vertexShaderSourceCode)
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderSourceCode);
     gl.compileShader(vertexShader);
-    console.log(vertexShader)
+   
     const fragmentShaderSourceCode = `#version 300 es
     precision mediump float;
     
-    out vec4 outputColor;
+    in vec3 fragColor;        
+    out vec4 outputColor;     
     
-    void mian(){
-        outputColor = vec4(0.294,0.0,0.51,1.0);
+    void main() {
+      outputColor = vec4(fragColor, 1.0);  
     }`;
+
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader,fragmentShaderSourceCode)
-    gl.compileShader(fragmentShader)
-    console.log(fragmentShader)
-    
-    const triangleProgram = gl.createProgram();
-    gl.attachShader(triangleProgram,vertexShader)
-    gl.attachShader(triangleProgram,fragmentShader)
-    gl.linkProgram(triangleProgram)
-    const vertexPostionAttributeLocation = gl.getAttribLocation(triangleProgram,'vertexPosition')
-    gl.useProgram(triangleProgram)
-    gl.enableVertexAttribArray(vertexPostionAttributeLocation)
-    gl.vertexAttribPointer(vertexPostionAttributeLocation,2,gl.FLOAT,false,0,0)
-    gl.drawArrays(gl.TRIANGLES,0,3)
-    
+    gl.shaderSource(fragmentShader, fragmentShaderSourceCode);
+    gl.compileShader(fragmentShader);
+
+    const TriangleProgram = gl.createProgram();
+    gl.attachShader(TriangleProgram, vertexShader);
+    gl.attachShader(TriangleProgram, fragmentShader);
+    gl.linkProgram(TriangleProgram);
+
+    const vertexPositionAttributeLocation = gl.getAttribLocation(TriangleProgram, 'vertexPosition');
+    gl.vertexAttribPointer(vertexPositionAttributeLocation, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(vertexPositionAttributeLocation);
+
+    const vertexColorAttributeLocation = gl.getAttribLocation(TriangleProgram, 'vertexColor');
+    gl.vertexAttribPointer(vertexColorAttributeLocation, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(vertexColorAttributeLocation);
+
+    gl.useProgram(TriangleProgram);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
-triangle()
+
+try {
+    triangle();
+} catch (e) {
+    alert(`Uncaught JavaScript exception: ${e}`);
+}
